ade2c0a9b2b9167aa31d6bf9541be1e5c70f45f3
Jan Feitsma <jan.feitsma@gmail.com>
2023-04-26 10:53:38 +0200

    base: setup base headers, literal copy of other repo, relative paths etc still need to be fixed

---
+++ base/abstract_interface.hpp
@@ -0,0 +1,30 @@
+#ifndef _BEAST_MRA_BASE_INTERFACE_HPP
+#define _BEAST_MRA_BASE_INTERFACE_HPP
+
+template <typename Ti, typename Tp, typename Ts, typename To, typename Tl>
+class MRAInterface
+{
+public:
+
+    using InputType = Ti;
+    using ParamsType = Tp;
+    using StateType = Ts;
+    using OutputType = To;
+    using LocalType = Tl;
+
+    MRAInterface() {};
+    ~MRAInterface() {};
+
+    virtual int tick(
+        double            timestamp,   // simulation timestamp, seconds since start of simulation
+        InputType  const &input,       // input data, type generated from Input.proto
+        ParamsType const &params,      // configuration parameters, type generated from Params.proto
+        StateType        &state,       // state data, type generated from State.proto
+        OutputType       &output,      // output data, type generated from Output.proto
+        LocalType        &local        // local/diagnostics data, type generated from Local.proto
+    ) = 0;
+
+}; // template class MRAInterface
+
+#endif // _BEAST_MRA_BASE_INTERFACE_HPP
+
---
+++ base/test_factory.hpp
@@ -0,0 +1,102 @@
+#ifndef _BEAST_MRA_TEST_FACTORY_HPP
+#define _BEAST_MRA_TEST_FACTORY_HPP
+
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+using namespace ::testing;
+
+#include <nlohmann/json.hpp>
+#include <google/protobuf/util/json_util.h>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+
+#define MRA_ROOT std::string("/home/jan/hobby/BEAST/MRA") // TODO make robust
+
+namespace TestFactory
+{
+
+std::string read_filename_as_string(std::string filename)
+{
+    std::ifstream infile(MRA_ROOT + "/" + filename);
+    if (!infile)
+    {
+        std::filesystem::path cwd = std::filesystem::current_path();
+        throw std::runtime_error("file " + filename + " not found at " + std::string(cwd));
+    }
+    std::ostringstream sstr;
+    sstr << infile.rdbuf();
+    return sstr.str();
+}
+
+template <typename T>
+std::string convert_proto_to_json_str(T const &tproto)
+{
+    std::string result;
+    auto r = google::protobuf::util::MessageToJsonString(tproto, &result);
+    if (!r.ok())
+    {
+        throw std::runtime_error("failed to convert protobuf struct to json");
+    }
+    return result;
+}
+
+template <typename T>
+void convert_json_to_proto(nlohmann::json const &j, std::string key, T &tproto)
+{
+    if (j.contains(key))
+    {
+        // data in the test vector file is either a file reference of convertable json data
+        if (j[key].is_string())
+        {
+            // assume file reference, load
+            auto r = google::protobuf::util::JsonStringToMessage(read_filename_as_string(j[key]), &tproto);
+            if (!r.ok())
+            {
+                throw std::runtime_error("failed to convert json from file " + std::string(j[key]) + " to protobuf struct");
+            }
+        }
+        else
+        {
+            // convert json value (via string)
+            std::string tmp_str = nlohmann::to_string(j[key]);
+            auto r = google::protobuf::util::JsonStringToMessage(tmp_str, &tproto);
+            if (!r.ok())
+            {
+                throw std::runtime_error("failed to convert tmp json string to protobuf " + key + " struct: '" + tmp_str + "'");
+            }
+        }
+    }
+}
+
+template <typename Tc>
+typename Tc::OutputType run_testvector(std::string tv_filename)
+{
+    // Arrange types
+    auto m = Tc();
+    auto input = typename Tc::InputType();
+    auto params = typename Tc::ParamsType();
+    auto expected_output = typename Tc::OutputType();
+    auto actual_output = typename Tc::OutputType();
+
+    // Act - load testvector
+    std::string js = read_filename_as_string(tv_filename);
+    nlohmann::json j = nlohmann::json::parse(js);
+    convert_json_to_proto(j, "Input", input);
+    convert_json_to_proto(j, "Params", params);
+    convert_json_to_proto(j, "Output", expected_output);
+
+    // Act - tick
+    int error_value = m.tick(input, actual_output);
+
+    // Assert
+    EXPECT_EQ(error_value, 0);
+    EXPECT_EQ(convert_proto_to_json_str(actual_output), convert_proto_to_json_str(expected_output));
+
+    // Return (for further checks) in test instance
+    return actual_output;
+}
+
+}; // namespace TestFactory
+
+#endif // _BEAST_MRA_TEST_FACTORY_HPP
