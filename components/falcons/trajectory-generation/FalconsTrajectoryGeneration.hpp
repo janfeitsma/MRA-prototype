506feadf0762067f45cef2dd94cd612eed643482
Jan Feitsma <jan.feitsma@gmail.com>
2023-05-13 16:24:26 +0200

    falcons/trajectory-generation: generated code

---
+++ components/falcons/trajectory-generation/FalconsTrajectoryGeneration.hpp
@@ -0,0 +1,82 @@
+// this file was produced by MRA-codegen.py from template_instance.hpp
+// it should NOT be modified by user
+
+#ifndef _MRA_FALCONS_TRAJECTORY_GENERATION_HPP
+#define _MRA_FALCONS_TRAJECTORY_GENERATION_HPP
+
+#include "abstract_interface.hpp"
+#include "params_loader.hpp"
+#include <google/protobuf/empty.pb.h>
+
+#include "components/falcons/trajectory-generation/interface/Input.pb.h"
+#include "components/falcons/trajectory-generation/interface/Output.pb.h"
+
+
+namespace MRA::FalconsTrajectoryGeneration
+{
+
+typedef MRA::FalconsTrajectoryGeneration::Input InputType;
+typedef google::protobuf::Empty ParamsType; // no .proto -> unused
+typedef google::protobuf::Empty StateType; // no .proto -> unused
+typedef MRA::FalconsTrajectoryGeneration::Output OutputType;
+typedef google::protobuf::Empty LocalType; // no .proto -> unused
+
+
+class FalconsTrajectoryGeneration: public MRAInterface<InputType, ParamsType, StateType, OutputType, LocalType>
+{
+public:
+    FalconsTrajectoryGeneration() {};
+    ~FalconsTrajectoryGeneration() {};
+
+    // user implementation
+    int tick(
+        double            timestamp,   // simulation timestamp, seconds since start of simulation
+        InputType  const &input,       // input data, type generated from Input.proto
+        ParamsType const &params,      // configuration parameters, type generated from Params.proto
+        StateType        &state,       // state data, type generated from State.proto
+        OutputType       &output,      // output data, type generated from Output.proto
+        LocalType        &local        // local/diagnostics data, type generated from Local.proto
+    );
+
+    // make default configuration easily accessible
+    ParamsType defaultParams() const
+    {
+        return MRA::LoadDefaultParams<ParamsType>("components/falcons/trajectory-generation/interface/DefaultParams.json");
+    };
+
+    // allow omitting arguments, useful for testing and code brevity
+    int tick()
+    {
+        StateType s;
+        OutputType o;
+        LocalType l;
+        return tick(0.0, InputType(), defaultParams(), s, o, l);
+    };
+
+    int tick(
+        InputType  const &input,
+        OutputType       &output
+    )
+    {
+        StateType s;
+        LocalType l;
+        return tick(0.0, input, defaultParams(), s, output, l);
+    };
+
+    int tick(
+        InputType  const &input,
+        ParamsType const &params,
+        OutputType       &output
+    )
+    {
+        StateType s;
+        LocalType l;
+        return tick(0.0, input, params, s, output, l);
+    };
+
+}; // class FalconsTrajectoryGeneration
+
+} // namespace MRA::FalconsTrajectoryGeneration
+
+#endif
+
---
+++ components/falcons/trajectory-generation/tick.cpp
@@ -0,0 +1,34 @@
+// this file was produced by MRA-codegen.py from template_tick.cpp
+// with the intent of letting user add the implementation here
+
+// generated component header:
+#include "FalconsTrajectoryGeneration.hpp"
+
+// dependent (generated) component headers:
+#include "FalconsVelocityControl.hpp"
+
+using namespace MRA;
+
+// custom includes, if any
+// ...
+
+
+int FalconsTrajectoryGeneration::FalconsTrajectoryGeneration::tick
+(
+    double            timestamp,   // simulation timestamp, seconds since start of simulation
+    InputType  const &input,       // input data, type generated from Input.proto
+    ParamsType const &params,      // configuration parameters, type generated from Params.proto
+    StateType        &state,       // state data, type generated from State.proto
+    OutputType       &output,      // output data, type generated from Output.proto
+    LocalType        &local        // local/diagnostics data, type generated from Local.proto
+)
+{
+    int error_value = 0;
+
+    // user implementation goes here
+
+
+
+    return error_value;
+}
+
