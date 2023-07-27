54418844cdfcaf7dc8efbfe05097a64b4bedaf17
Jan Feitsma <jan.feitsma@gmail.com>
2023-07-27 18:33:27 +0200

    MRA-build.py: add options --dryrun and --scope

--- MRA-build.py
+++ MRA-build.py
@@ -4,12 +4,23 @@
 Build and optionally test. 
 
 Default uses bazel build system.
-TODO: option for cmake?
+
+Examples:
+* run a full build
+  $ ./MRA-build.py
+* display the commands to run a full build and test
+  $ ./MRA-build.py -n -t
+     bazel build --color=yes //...
+     bazel test //... --test_output all --nocache_test_results
+* commands to build only one component
+  $ ./MRA-build.py -n -s alive
+     bazel build --color=yes //robotsports/proof-is-alive
 '''
 
 # python modules
 import sys
 import os
+import glob
 import pathlib
 import subprocess
 import argparse
@@ -19,13 +30,15 @@ import argparse
 MRA_ROOT = pathlib.Path(__file__).parent.resolve()
 DEBUG_OPTIONS = '--subcommands --verbose_failures --sandbox_debug'
 TEST_OPTIONS = '--test_output all --nocache_test_results'
+DEFAULT_SCOPE = '...'
 
 
 class BazelBuilder():
-    def __init__(self, verbose: str = True, debug: str = False):
+    def __init__(self, verbose: bool = True, debug: bool = False, dryrun: bool = False):
         self.verbose = verbose
         self.debug = debug
-    def run(self, clean: str = False, test: str = False) -> None:
+        self.dryrun = dryrun
+    def run(self, clean: bool = False, test: bool = False, scope: str = DEFAULT_SCOPE) -> None:
         """
         Perform the work:
         1. clean (optional)
@@ -34,22 +47,26 @@ class BazelBuilder():
         """
         if clean:
             self.run_clean()
-        self.run_build()
+        self.run_build(scope)
         if test:
-            self.run_test()
+            self.run_test(scope)
     def run_clean(self) -> None:
         self.run_cmd('bazel clean --color=yes')
-    def run_build(self) -> None:
+    def run_build(self, scope : list) -> None:
         cmd_parts = ['bazel', 'build']
         if self.debug:
             cmd_parts.append(DEBUG_OPTIONS)
-        cmd_parts += ['--color=yes', '//...']
-        self.run_cmd(' '.join(cmd_parts))
-    def run_test(self) -> None:
-        cmd = f'bazel test //... ' + TEST_OPTIONS
-        self.run_cmd(cmd)
+        for s in scope:
+            self.run_cmd(' '.join(cmd_parts + ['--color=yes', f'//{s}']))
+    def run_test(self, scope : list) -> None:
+        for s in scope:
+            cmd = f'bazel test //{s} ' + TEST_OPTIONS
+            self.run_cmd(cmd)
     def run_cmd(self, cmd: str) -> None:
         extra_opts = {}
+        if self.dryrun:
+            print(cmd)
+            return
         if self.verbose:
             print(f'running command: {cmd}')
         else:
@@ -63,11 +80,42 @@ class BazelBuilder():
             sys.exit(1)
 
 
+def is_component(d: str) -> bool:
+    """Guess if a folder is a component. Allow nesting."""
+    return os.path.isdir(os.path.join(d, 'interface'))
+
+
+def find_components() -> list:
+    """Find all component folders."""
+    result = glob.glob(os.path.join('components/**'), recursive=True) # relative paths
+    result = [d for d in result if is_component(d)]
+    return result
+
+
+def resolve_scope(iscope : str) -> list:
+    """
+    Resolve build- and optional test scope as a list of strings to pass to bazel, one at a time.
+    Make a guess on user input, which may be a comma separated list of strings. See general examples.
+    """
+    if iscope == '...':
+        return ['...'] # full scope, default
+    components = find_components()
+    result = set()
+    for s in iscope.split(','):
+        candidates = [c for c in components if c.endswith(s)]
+        if len(candidates) == 1:
+            result.add(candidates[0] + ':all')
+        else:
+            raise Exception(f'failed to resolve component for "{s}", candidates: ' + str(candidates))
+    return list(result)
+
+
 def main(**kwargs) -> None:
     """Perform the work."""
     os.chdir(MRA_ROOT)
-    b = BazelBuilder(verbose = not kwargs.get('quiet'), debug = kwargs.get('debug'))
-    b.run(clean = kwargs.get('clean'), test = kwargs.get('test'))
+    b = BazelBuilder(verbose = not kwargs.get('quiet'), debug = kwargs.get('debug'), dryrun = kwargs.get('dryrun'))
+    scope = resolve_scope(kwargs.get('scope'))
+    b.run(clean = kwargs.get('clean'), test = kwargs.get('test'), scope = scope)
 
 
 def parse_args(args: list) -> argparse.Namespace:
@@ -77,8 +125,10 @@ def parse_args(args: list) -> argparse.Namespace:
     class CustomFormatter(argparse.ArgumentDefaultsHelpFormatter, argparse.RawDescriptionHelpFormatter):
         pass
     parser = argparse.ArgumentParser(description=descriptionTxt, epilog=exampleTxt, formatter_class=CustomFormatter)
+    parser.add_argument('-s', '--scope', help='build scope, comma-separated lists of (brief) component names', type=str, default=DEFAULT_SCOPE)
     parser.add_argument('-c', '--clean', help='start with cleaning', action='store_true')
     parser.add_argument('-t', '--test', help='also run tests', action='store_true')
+    parser.add_argument('-n', '--dryrun', help='only print commands', action='store_true')
     parser.add_argument('-d', '--debug', help='enable some debug flags', action='store_true')
     parser.add_argument('-q', '--quiet', help='suppress output on what is happening', action='store_true')
     return parser.parse_args(args)
