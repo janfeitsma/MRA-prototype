afbb4edc97798478a18c086a8301e250c2a372c2
Jan Feitsma <jan.feitsma@gmail.com>
2023-08-01 23:14:03 +0200

    falcons/velocity_control: fix the new test demo_protobuf.py

--- components/falcons/velocity_control/test/demo_protobuf.py
+++ components/falcons/velocity_control/test/demo_protobuf.py
@@ -8,21 +8,24 @@ Adding some test coverage around protobuf python build.
 If it works for this component, then others are also likely covered.
 '''
 
+
 # python modules
 import json
+from google.protobuf import json_format
 
 # component modules
-import params_pb2
+import components.falcons.velocity_control.interface.Params_pb2 as Params_pb2
 
 
 
 def main():
-    # create an object of type Params
-    p = params_pb2.Params()
-    # load default values from json
-    #TODO
-    # print
-    print(p)
+    file_path = 'components/falcons/velocity_control/interface/DefaultParams.json'
+    with open(file_path, 'r') as file:
+        json_data = json.load(file)
+    protobuf_message = Params_pb2.Params()
+    json_format.ParseDict(json_data, protobuf_message)
+    print(f'Successfully read Params from {file_path}, first lines follow:')
+    print('\n'.join(str(protobuf_message).splitlines()[:7]))
 
 
 if __name__ == '__main__':
