c621881855bdfd2d9a6f366716f43a5046a8520c
Jan Feitsma <jan.feitsma@gmail.com>
2023-09-03 20:58:06 +0200

    logging: refactor tests and helpers

--- components/robotsports/proof_is_alive/test/test_mra_logger.cpp
+++ components/robotsports/proof_is_alive/test/test_mra_logger.cpp
@@ -1,7 +1,7 @@
 // Test suite for MRA logger.
 
 // Include testframework and system includes
-#include "gtest/gtest.h"
+#include "test_mra_logger_helpers.hpp"
 #include <filesystem>
 
 // System under test
@@ -10,41 +10,6 @@
 // Auxiliary component (rather than defining a dummy component)
 #include "RobotsportsProofIsAlive.hpp"
 
-// Mock for MRA::Logging::logFolder
-namespace MRA::Logging {
-    std::string logFolder() {
-        return "/tmp/test_mra_logging";
-    }
-}
-
-// Define for log folder
-#define LOG_FOLDER "/tmp/test_mra_logging"
-
-// Pre-test cleanup
-class TestFixture : public ::testing::Test {
-protected:
-    void SetUp() override {
-        cleanupLogFolder();
-    }
-
-    void TearDown() override {
-        cleanupLogFolder();
-    }
-
-    void cleanupLogFolder() {
-        std::string logFolderPath = LOG_FOLDER;
-        // Check if the directory exists
-        if (std::filesystem::exists(logFolderPath)) {
-            // Remove all files and subdirectories within the log folder
-            for (const auto &entry : std::filesystem::directory_iterator(logFolderPath)) {
-                if (entry.is_regular_file() || entry.is_directory()) {
-                    std::cout << "std::filesystem::remove_all(" << entry.path() << ")";
-                    //std::filesystem::remove_all(entry.path());
-                }
-            }
-        }
-    }
-};
 
 // Helper function: run a single tick
 void runtick() {
@@ -53,51 +18,13 @@ void runtick() {
     EXPECT_EQ(error_value, 0);
 }
 
-// Helper function: check if log folder exists
-bool check_log_folder_existing(std::string log_folder = LOG_FOLDER) {
-    if (!std::filesystem::exists(log_folder)) return false;
-    return std::filesystem::is_directory(log_folder);
-}
-
-// Helper function: count number of produced log files
-int count_log_files(std::string log_folder = LOG_FOLDER) {
-    try {
-        int count = 0;
-        // Use std::filesystem to iterate through files in the log folder
-        for (const auto &entry : std::filesystem::directory_iterator(log_folder)) {
-            if (entry.is_regular_file()) {
-                count++;
-            }
-        }
-        return count;
-    } catch (const std::filesystem::filesystem_error &e) {
-        // Handle any errors that might occur during folder iteration
-        return -1; // Return -1 in case of any error
-    }
-}
-
-// Helper function: count number of produced log lines
-int count_log_lines(std::string filename) {
-    std::ifstream fh(filename);
-    int count = 0;
-    if (fh.is_open()) {
-        std::string line;
-        while (std::getline(fh, line)) {
-            count++;
-        }
-        fh.close();
-    }
-    return count;
-}
-
 // Default tick shall produce logging
 TEST_F(TestFixture, defaultLogging) {
-    MRA::Logging::control::resetConfiguration();
     // Act
     runtick();
 
     // Assert
-    std::string expected_log_file = LOG_FOLDER "/robotsports/proof_is_alive.log";
+    std::string expected_log_file = LOG_FOLDER_TEST "/RobotsportsProofIsAlive.log";
     EXPECT_TRUE(check_log_folder_existing());
     EXPECT_EQ(count_log_files(), 1);
     EXPECT_TRUE(std::filesystem::exists(expected_log_file));
---
+++ components/robotsports/proof_is_alive/test/test_mra_logger_helpers.hpp
@@ -0,0 +1,51 @@
+#ifndef _MRA_TEST_MRA_LOGGER_HELPERS_HPP
+#define _MRA_TEST_MRA_LOGGER_HELPERS_HPP
+
+#include "logging.hpp"
+#include "gtest/gtest.h"
+#include <filesystem>
+
+
+// Mock/manipulate MRA::Logging::logFolder, leave default intact
+#define LOG_FOLDER_TEST "/tmp/test_mra_logging"
+
+// Helper function: test-specific configuration
+// (to not affect standard log folder, to enable hotflush)
+MRA::Datatypes::LogControl testConfiguration();
+
+// Pre-test cleanup and configuration
+class TestFixture : public ::testing::Test {
+protected:
+    void SetUp() override {
+        MRA::Logging::control::setConfiguration(testConfiguration());
+        cleanupLogFolder();
+    }
+
+    void TearDown() override {
+        cleanupLogFolder();
+    }
+
+    void cleanupLogFolder() {
+        std::string logFolderPath = LOG_FOLDER_TEST;
+        // Check if the directory exists
+        if (std::filesystem::exists(logFolderPath)) {
+            std::filesystem::remove_all(logFolderPath);
+        }
+    }
+};
+
+// Helper function: run a single tick
+void runtick();
+
+// Helper function: check if log folder exists
+bool check_log_folder_existing(std::string log_folder = LOG_FOLDER_TEST);
+
+// Helper function: count number of produced log files
+int count_log_files(std::string log_folder = LOG_FOLDER_TEST);
+
+// Helper function: count number of produced log lines
+int count_log_lines(std::string filename);
+
+
+#endif // #ifndef _MRA_TEST_MRA_LOGGER_HELPERS_HPP
+
