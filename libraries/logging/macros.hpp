e8ec8b717f5ff682fd8a299e82de7f02caff6554
Jan Feitsma <jan.feitsma@gmail.com>
2023-09-09 11:46:10 +0200

    logging: extend the macros

--- libraries/logging/macros.hpp
+++ libraries/logging/macros.hpp
@@ -8,6 +8,14 @@
 
 #define MRA_LOG_CRITICAL(...) MRA::Logging::backend::MraLogger::getInstance()->log(\
     MRA::Logging::backend::source_loc(__FILE__, __LINE__, __FUNCTION__) , MRA::Logging::CRITICAL, __VA_ARGS__ )
+#define MRA_LOG_ERROR(...) MRA::Logging::backend::MraLogger::getInstance()->log(\
+    MRA::Logging::backend::source_loc(__FILE__, __LINE__, __FUNCTION__) , MRA::Logging::ERROR, __VA_ARGS__ )
+#define MRA_LOG_WARNING(...) MRA::Logging::backend::MraLogger::getInstance()->log(\
+    MRA::Logging::backend::source_loc(__FILE__, __LINE__, __FUNCTION__) , MRA::Logging::WARNING, __VA_ARGS__ )
+#define MRA_LOG_INFO(...) MRA::Logging::backend::MraLogger::getInstance()->log(\
+    MRA::Logging::backend::source_loc(__FILE__, __LINE__, __FUNCTION__) , MRA::Logging::INFO, __VA_ARGS__ )
+#define MRA_LOG_DEBUG(...) MRA::Logging::backend::MraLogger::getInstance()->log(\
+    MRA::Logging::backend::source_loc(__FILE__, __LINE__, __FUNCTION__) , MRA::Logging::DEBUG, __VA_ARGS__ )
 
 #endif // #ifndef _MRA_LIBRARIES_LOGGING_MACROS_HPP
 
