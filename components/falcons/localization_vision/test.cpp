dc174f0396fa52cb02fb282ad24b19d9f1ac657b
Jan Feitsma <jan.feitsma@gmail.com>
2023-09-22 12:20:48 +0200

    falcons/localization_vision: add MRA_TRACE_TEST_FUNCTION

--- components/falcons/localization_vision/test.cpp
+++ components/falcons/localization_vision/test.cpp
@@ -23,6 +23,7 @@ using namespace MRA;
 // Basic tick shall run OK and return error_value 0.
 TEST(FalconsLocalizationVisionTest, basicTick)
 {
+    MRA_TRACE_TEST_FUNCTION();
     // Arrange
     auto m = FalconsLocalizationVision::FalconsLocalizationVision();
 
@@ -36,6 +37,7 @@ TEST(FalconsLocalizationVisionTest, basicTick)
 // Reference floor generation
 TEST(FalconsLocalizationVisionTest, referenceFloor)
 {
+    MRA_TRACE_TEST_FUNCTION();
     // The floor is stored in state.
     // A pretty version with grid lines and input pixels (if present, not in this test)
     // is stored in local, but only if debug is enabled.
@@ -83,6 +85,7 @@ TEST(FalconsLocalizationVisionTest, referenceFloor)
 // Calculation/scoring function
 TEST(FalconsLocalizationVisionTest, calc000)
 {
+    MRA_TRACE_TEST_FUNCTION();
     // Arrange
     auto m = FalconsLocalizationVision::FalconsLocalizationVision();
     auto params = m.defaultParams();
@@ -134,16 +137,19 @@ TEST(FalconsLocalizationVisionTest, perfectFit)
 // This is data driven -> json test case
 TEST(FalconsLocalizationVisionTest, jsonTest1PerfectFit)
 {
+    MRA_TRACE_TEST_FUNCTION();
     auto output = TestFactory::run_testvector<FalconsLocalizationVision::FalconsLocalizationVision>(std::string("components/falcons/localization_vision/testdata/test1_perfect_fit.json"));
 }
 TEST(FalconsLocalizationVisionTest, jsonTest2ShiftXY)
 {
+    MRA_TRACE_TEST_FUNCTION();
     auto output = TestFactory::run_testvector<FalconsLocalizationVision::FalconsLocalizationVision>(std::string("components/falcons/localization_vision/testdata/test2_shift_xy.json"));
 }
 
 // Integration bugfixes
 TEST(FalconsLocalizationVisionTest, jsonTest3GrabsR5BadInit)
 {
+    MRA_TRACE_TEST_FUNCTION();
     auto output = TestFactory::run_testvector<FalconsLocalizationVision::FalconsLocalizationVision>(std::string("components/falcons/localization_vision/testdata/test3_grabs_r5_20191219_210335_bad_init.json"));
 }
 
