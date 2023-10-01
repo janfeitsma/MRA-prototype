543fdec0d1b1075a9b26baa6acd4a8c99647b21a
Jurge vanEijck <jurge.van.eijck@gmail.com>
2023-10-01 11:23:03 +0200

    remove unnecarry typedef

--- base/abstract_interface.hpp
+++ base/abstract_interface.hpp
@@ -6,27 +6,20 @@
 namespace MRA
 {
 
-template <typename Ti, typename Tp, typename Ts, typename To, typename Tl>
+template <typename T_InputType, typename T_ParamsType, typename T_StateType, typename T_OutputType, typename T_LocalType>
 class MRAInterface
 {
 public:
-
-    using InputType = Ti;
-    using ParamsType = Tp;
-    using StateType = Ts;
-    using OutputType = To;
-    using LocalType = Tl;
-
     MRAInterface() {};
     ~MRAInterface() {};
 
     virtual int tick(
         google::protobuf::Timestamp timestamp,   // absolute timestamp
-        InputType  const           &input,       // input data, type generated from Input.proto
-        ParamsType const           &params,      // configuration parameters, type generated from Params.proto
-        StateType                  &state,       // state data, type generated from State.proto
-        OutputType                 &output,      // output data, type generated from Output.proto
-        LocalType                  &local        // local/diagnostics data, type generated from Local.proto
+        T_InputType  const           &input,       // input data, type generated from Input.proto
+		T_ParamsType const           &params,      // configuration parameters, type generated from Params.proto
+		T_StateType                  &state,       // state data, type generated from State.proto
+		T_OutputType                 &output,      // output data, type generated from Output.proto
+		T_LocalType                  &local        // local/diagnostics data, type generated from Local.proto
     ) = 0;
 
 }; // template class MRAInterface
